e278a0cafe1d96af9343d8c2a978bd388c2c8c9f
Aldevisign <aldevisign@gmail.com>
2022-02-10 21:00:09 +0100

    Correction du JSON avec de la chance

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-let requestUrl = "test";
+// https://raw.githubusercontent.com/Alexandreblnqro/Webeorit/master/src/translation.json
+
+var request = new XMLHttpRequest();
+request.open(
+    "GET",
+    "https://raw.githubusercontent.com/Alexandreblnqro/Webeorit/master/src/translation.json",
+    false
+);
+request.onload = function() {
+    var data = JSON.parse(request.responseText);
+    console.log(data);
+};
+request.send();
